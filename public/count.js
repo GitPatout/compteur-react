e1ba18622948d335a6804a8743c02fd15eb55f0e
GitPatout <patout.julien.02@gmail.com>
2022-05-10 09:59:37 +0200

    nouvelle façon de faire un compteur

---
+++ public/count.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const Count = () => {
+    const [count, setCount] = useState(0);
+
+    const minus = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    }
+
+    const reset = () => {
+        setCount(0);
+    }
+
+    return (
+        <>
+
+            <button onClick={minus}>-</button>
+            {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={reset}>Reset</button>
+
+        </>
+    )
+}
